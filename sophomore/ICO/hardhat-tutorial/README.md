9043544d661045b40fc51f4542ab0ee10e58d83d
Ziion <mdzuhaib444@gmail.com>
2022-09-02 05:47:00 -0700

    Sophomore: ICO

---
+++ sophomore/ICO/hardhat-tutorial/README.md
@@ -0,0 +1,3 @@
+# Sample Hardhat Project
+
+Goerli Contract address - 0xC663Ac9DcDfBE859044c01475Ca9d44d066B3B0f
---
+++ sophomore/ICO/hardhat-tutorial/constants /index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xF55e3d8A4568cf01cBe088cE5a1738201df40A7b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
